fcb55e90e25562457c0f5224bf2ba834ff67ca37
Gabriel Miranda <ggmiranda@uc.cl>
2023-03-25 01:09:26 -0300

    first try of the model

--- angiogenesis_model/functions/d_logistic_decay.m
+++ angiogenesis_model/functions/d_logistic_decay.m
@@ -1,5 +1,5 @@
-function [dld] = d_logistic_decay(L,b,c,y)
-    num = L*b*c*exp(-c*y);
+function [dld] = d_logistic_decay(a,b,c,y)
+    num = a*b*c*exp(-c*y);
     den = (1+b*exp(-c*y))^2;
     dld=num/den;
 end

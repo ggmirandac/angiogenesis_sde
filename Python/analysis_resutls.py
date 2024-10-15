f07e0237554283fac4a762351c42fb5fb2050a9f
ggmirandac <ggmiranda@uc.cl>
2024-10-15 13:02:52 -0300

    Refactor batch_histogram_hurst function to add optional parameters for xlims and kde

--- Python/analysis_resutls.py
+++ Python/analysis_resutls.py
@@ -45,7 +45,9 @@ def batch_histogram_delta(folder: str,
     
     
 def batch_histogram_hurst(folder: str,
-                    Delta: float = None)->None:
+                    Delta: float = None,
+                    xlims = [0,100],
+                    kde = True)->None:
     """
     Plots the histogram using a gaussian kernel density estimation 
     - The constant value is the Hurst parameter and the changing is based on the delta parameter
@@ -67,33 +69,55 @@ def batch_histogram_hurst(folder: str,
     df_to_hist.reset_index(inplace=True, drop = True)
     # print(df_to_hist)
     color_map = plt.get_cmap('coolwarm', len(df_to_hist))
-
-    for i in df_to_hist.index:
-            
-        data = df_to_hist.at[i, 'Array']
-        hurst = df_to_hist.at[i, 'H']
-        non_nan_data = data[~np.isnan(data)]
-        #print(non_nan_data)
-        por_nan = (len(data) - len(non_nan_data))/len(data)
-        kde = stats.gaussian_kde(non_nan_data)
-        str_label = f'H = {float(hurst):.3f}; {por_nan:.2f} non-reaching'
-        ax.plot(linspa, kde(linspa), label = str_label, color = color_map(i)) 
+    
+    if kde:
+        for i in df_to_hist.index:
+                
+            data = df_to_hist.at[i, 'Array']
+            hurst = df_to_hist.at[i, 'H']
+            non_nan_data = data[~np.isnan(data)]
+            #print(non_nan_data)
+            non_nan_data_lims = non_nan_data[non_nan_data < xlims[1]]
+            por_nan = (len(data) - len(non_nan_data))/len(non_nan_data_lims)
+            kde = stats.gaussian_kde(non_nan_data_lims, bw_method='silverman')
+            str_label = f'H = {float(hurst):.3f}; {por_nan:.2f} non-reaching'
+            ax.plot(linspa, kde(linspa), label = str_label, color = color_map(i)) 
+    else:
+        for i in df_to_hist.index:
+                
+            data = df_to_hist.at[i, 'Array']
+            hurst = df_to_hist.at[i, 'H']
+            non_nan_data = data[~np.isnan(data)]
+            non_nan_data_lims = non_nan_data[non_nan_data < xlims[1]]
+            #print(non_nan_data)
+            por_nan = (len(data) - len(non_nan_data_lims))/len(non_nan_data_lims)
+            str_label = f'H = {float(hurst):.3f}; {por_nan:.2f} non-reaching'
+            ax.hist(non_nan_data_lims, bins = 1000, alpha = 0.5, label = str_label, color = color_map(i))
     ax.legend()
     ax.set_title(f'Delta : {Delta}')    
-    ax.legend(bbox_to_anchor = (1.3, 1))
+    ax.set_xlim(*xlims)
+    ax.legend(bbox_to_anchor = (1, 0.9))
     plt.show()
 #%%
 if __name__ == '__main__':
-    batch_histogram_delta('delta_change_035_from_cluster', 
-                          H = 0.35)
-    batch_histogram_delta('delta_change_05_from_cluster',
-                          H = 0.5)
-    batch_histogram_delta('delta_change_065_from_cluster',
-                        H = 0.65)
-    batch_histogram_hurst('h_change_all_from_cluster',
-                          Delta = 1)
-    batch_histogram_hurst('h_change_close_from_cluster',
-                          Delta = 1)
-    batch_histogram_hurst('h_change_closer_from_cluster',
-                          Delta = 1)
+    # batch_histogram_delta('delta_change_035_from_cluster', 
+    #                       H = 0.35)
+    # batch_histogram_delta('delta_change_05_from_cluster',
+    #                       H = 0.5)
+    # batch_histogram_delta('delta_change_065_from_cluster',
+    #                     H = 0.65)
+    # batch_histogram_hurst('h_change_all_from_cluster',
+    #                       Delta = 1)
+    # batch_histogram_hurst('h_change_close_from_cluster',
+    #                       Delta = 1)
+    # batch_histogram_hurst('h_change_closer_from_cluster',
+    #                       Delta = 1)
+    batch_histogram_hurst('h_change_all_wall_at_1_from_cluster',
+                          Delta = 1,
+                          xlims=[0,10],
+                          kde = True)
+    batch_histogram_hurst('h_change_all_wall_at_10_from_cluster',
+                          Delta = 1,
+                          xlims=[0,100],
+                          kde = True)
 # %%

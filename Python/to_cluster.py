c61753218039da5a290c451cb6434d0f3018f3f3
ggmirandac <ggmiranda@uc.cl>
2024-11-30 01:25:48 -0300

    a

--- Python/to_cluster.py
+++ Python/to_cluster.py
@@ -1,30 +1,31 @@
 #%% Running
-from angiosde import AngioSimulation, ConstantGradient, LinearGradient
+# from angiosde import AngioSimulation, ConstantGradient, LinearGradient
+from angiosde_limit import AngioSimulation, ConstantGradient, LinearGradient
 from os.path import join 
 import numpy as np
 import matplotlib.pyplot as plt
 # parameters
 
 n_reps = 10
-Hurst_index = 0.75
+Hurst_index = 0.5
 n_steps = 100_000
 dtau = .001
 delta = 1  # TODO: review the delta effect over the simulation
 mode = 'Simulate'
 
-# lin_gradient = LinearGradient(0.1, [0, 25], 0.01)
-const_gradient = ConstantGradient(0.1)
+# lin_gradient = LinearGradient(0.1, [0, 25], 25)
+# const_gradient = Linea(0.1)
 
-list_H =  [0.5]
+list_H =  [0.75]
 # for h in list_H:
 for h in list_H:
     A_sim = AngioSimulation(n_reps, h, n_steps, dtau, delta,
-                        xa=[0, 10_000],
+                        xa=[0, 25],
                         mode="Simulate",
                         wall=25, 
                         only_ht=True, 
-                        Grad=const_gradient)
-    A_sim.simulate(n_jobs=10)
+                        Grad=ConstantGradient(0.1))
+    A_sim.simulate(n_jobs=1)
 
     A_sim.plot_sprouts()
     # h_str = str(h).replace('.','_')
